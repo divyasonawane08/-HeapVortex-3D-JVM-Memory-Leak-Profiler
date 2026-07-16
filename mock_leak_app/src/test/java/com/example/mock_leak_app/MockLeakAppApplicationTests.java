9f7802c7ea3638d7891f145801b122f32d3240f1
Divya <divyasonawane08112000@gmail.com>
2026-07-16 09:55:28 +0530

    mock_leak_app

---
+++ mock_leak_app/src/test/java/com/example/mock_leak_app/MockLeakAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mock_leak_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockLeakAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
